476f6a3e3d416ad41944c468aed88d4bbd5a3dd7
moo-d <nazwa.salsa67.bila@gmail.com>
2024-02-15 10:35:16 +0700

    oopsie

--- commands/downloader/youtube.ts
+++ commands/downloader/youtube.ts
@@ -1,62 +1,78 @@
-import { Attributes, RunParams } from "../../src/Types"
-import { ytmp3, ytmp4 } from "../../src/Scraper"
+import { Attributes, RunParams } from "../../src/Types";
+import { ytmp3, ytmp4 } from "../../src/Scraper";
+
+interface BotRequest {
+  [key: string]: any;
+}
 
 export default {
   alias: ["yt"],
   desc: "youtube video/audio download",
   category: "downloader",
-  async run(
-    { m, bot }: RunParams,
-    { query, userId, chatId, args, messageId, isUrl }: Attributes
-  ) {
-    bot.onRequest = bot.onRequest ? bot.onRequest : {}
+  async run({ m, bot }: RunParams, { query, userId, chatId, args, messageId, isUrl }: Attributes): Promise<void> {
+    bot.onRequest = bot.onRequest || {};
+
+    const onRequest = bot.onRequest;
+
+    const cancelRequest = (): void => {
+      m.editMessageReplyMarkup({}, {
+        chat_id: chatId,
+        message_id: m.update.callback_query.message.message_id
+      });
+      if (onRequest[userId]) {
+        delete onRequest[userId];
+        m.reply('Permintaan download youtube dibatalkan.');
+      }
+    };
+
+    const handleAudio = async (): Promise<void> => {
+      cancelRequest();
+      m.editMessageReplyMarkup({}, {
+        chat_id: chatId,
+        message_id: m.update.callback_query.message.message_id
+      });
+      m.reply('Audio telah dipilih. Mohon tunggu, audio sedang dikirim...');
+      const res = await ytmp3(args[2]);
+      const { url } = res;
+      m.replyWithAudio(
+        { url: url },
+        { reply_to_message_id: messageId }
+      );
+    };
+
+    const handleVideo = async (): Promise<void> => {
+      cancelRequest();
+      m.editMessageReplyMarkup({}, {
+        chat_id: chatId,
+        message_id: m.update.callback_query.message.message_id
+      });
+      m.reply('Video telah dipilih. Mohon tunggu, video sedang dikirim...');
+      const res = await ytmp4(args[2]);
+      const { url } = res;
+      m.replyWithVideo(
+        { url: url },
+        { reply_to_message_id: messageId }
+      );
+    };
+
     if (query) {
       switch (args[1]) {
         case 'cancel':
-          m.editMessageReplyMarkup({}, {
-            chat_id: chatId,
-            message_id: m.update.callback_query.message.message_id
-          })
-          if (bot.onRequest[userId]) {
-            delete bot.onRequest[userId]
-            m.reply('Permintaan download youtube dibatalkan.')
-          }
-          break
+          cancelRequest();
+          break;
         case 'audio':
-          if (bot.onRequest[userId]) {
-            delete bot.onRequest[userId];
+          if (onRequest[userId]) {
+            handleAudio();
           }
-          m.editMessageReplyMarkup({}, {
-            chat_id: chatId,
-            message_id: m.update.callback_query.message.message_id
-          })
-          m.reply('Audio telah dipilih. Mohon tunggu, audio sedang dikirim...')
-          var res = await ytmp3(args[2])
-          var { url } = res
-          m.replyWithAudio(
-            { url: url },
-            { reply_to_message_id: messageId }
-          )
-          break
+          break;
         case 'video':
-          if (bot.onRequest[userId]) {
-            delete bot.onRequest[userId];
+          if (onRequest[userId]) {
+            handleVideo();
           }
-          m.editMessageReplyMarkup({}, {
-            chat_id: chatId,
-            message_id: m.update.callback_query.message.message_id
-          })
-          m.reply('Video telah dipilih. Mohon tunggu, video sedang dikirim...')
-          var res = await ytmp4(args[2])
-          var { url } = res
-          m.replyWithVideo(
-            { url: url },
-            { reply_to_message_id: messageId }
-          )
-          break
+          break;
         default:
-          if (bot.onRequest[userId]) {
-            delete bot.onRequest[userId];
+          if (onRequest[userId]) {
+            delete onRequest[userId];
           }
           if (isUrl(args[1])) {
             const button: any[][] = [
@@ -70,24 +86,24 @@ export default {
                   callback_data: `/youtube video ${args[1]}`
                 }
               ]
-            ]
+            ];
             m.reply('Apa yang ingin anda download?', {
               reply_markup: {
                 inline_keyboard: button,
               }
-            })
+            });
           } else {
-            m.reply('Tolong berikan url dengan benar!')
-            bot.onRequest[userId] = {
+            m.reply('Tolong berikan url dengan benar!');
+            onRequest[userId] = {
               command: "youtube"
-            }
+            };
           }
       }
     } else {
-      bot.onRequest[userId] = {
+      onRequest[userId] = {
         command: "youtube"
-      }
-      m.reply('Kirim linknya kak')
+      };
+      m.reply('Kirim linknya kak');
     }
   }
-}
+};
