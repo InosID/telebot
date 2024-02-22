5a2520f8a897a4a19c7dd5a5fb0ec8ad635eb831
Awa <75057738+moo-d@users.noreply.github.com>
2024-02-22 11:32:55 +0700

    Settings update
    - add Logger

--- settings.ts
+++ settings.ts
@@ -1,2 +1,38 @@
+import chalk from 'chalk';
 import dotenv from 'dotenv';
+
 dotenv.config();
+
+interface Logger {
+  connection: {
+    start: string;
+    error: {
+      invalid_token: string;
+      conflict: string;
+    };
+  };
+  command: {
+    receive: {
+      private: string;
+      group: string;
+    };
+  };
+}
+
+const LOGGER: Logger = {
+  connection: {
+    start: chalk.greenBright('Connected as %botname'),
+    error: {
+      invalid_token: chalk.red('[ERROR]') + ' Your token is not valid.',
+      conflict: ""
+    }
+  },
+  command: {
+    receive: {
+      private: `${chalk.cyan('[PRIVATE]')} ${chalk.green(' %username')} - %cmd`,
+      group: `${chalk.cyan('[GROUP]')} ${chalk.blue(' %gcname')}: ${chalk.green(' %username')} - %cmd`
+    }
+  }
+};
+
+export { LOGGER };
