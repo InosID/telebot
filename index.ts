6f020228c19eacab103ecbc7ad567edafde1aefe
Awa <75057738+moo-d@users.noreply.github.com>
2024-02-22 11:28:26 +0700

    little change
    :>

--- index.ts
+++ index.ts
@@ -1,25 +1,41 @@
-console.log('Starting...');
 import { spawn, ChildProcess } from 'child_process';
 import * as path from 'path';
+import express, { Application, Request, Response } from 'express';
+import nocache from './src/Utils/cache';
+
+const app: Application = express()
+const PORT = process.env.PORT || 3000
+
+console.log(require('./src/Database'))
+console.log('Starting...');
 
 let isProcessRunning: boolean = false;
 
-function startProcess(fileToStart: string): void {
+global.reloadFile = function(file: string = ''): void {
+  nocache(file, () => {
+    console.log(`File "${file}" has been updated!\nRestarting!`);
+    process.send?.("reset");
+  });
+};
+
+async function startProcess(fileToStart: string): Promise<void> {
   if (isProcessRunning) return;
   isProcessRunning = true;
 
   const processArguments: string[] = [path.join(__dirname, fileToStart), ...process.argv.slice(2)];
+
   const childProcess: ChildProcess = spawn(process.argv[0], processArguments, {
     stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
   });
 
-  childProcess.on('message', (data: any) => {
+  childProcess.on('message', async (data: string | number) => {
     console.log('[MESSAGE RECEIVED]', data);
+
     switch (data) {
       case 'reset':
         childProcess.kill();
         isProcessRunning = false;
-        startProcess(fileToStart);
+        await startProcess(fileToStart);
         break;
       case 'uptime':
         childProcess.send(process.uptime());
@@ -34,4 +50,22 @@ function startProcess(fileToStart: string): void {
   });
 }
 
-startProcess("src");
+async function main(): Promise<void> {
+  try {
+    await startProcess("src");
+  } catch (error) {
+    console.error('Error occurred:', error);
+    process.exit(1);
+  }
+}
+
+main();
+
+app.set("json spaces", 2);
+app.get("*", async (req: Request, res: Response) => {
+  res.json({
+    online: true,
+  });
+});
+
+app.listen(PORT, () => console.log(`Server running with port ${PORT}!`));
